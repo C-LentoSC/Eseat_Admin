d149c74ccafcd7f4c0a5f3a0fc82b2d26bdf140b
KayT3 <sithija.ap@gmail.com>
2024-12-19 01:57:15 +0530

    fixing errors 3

--- src/model/API.js
+++ src/model/API.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import {jwtDecode} from "jwt-decode";
 // Create Axios instance
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'https://mintberryco.com/api/',
+    // baseURL: 'http://127.0.0.1:8000/api/',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
