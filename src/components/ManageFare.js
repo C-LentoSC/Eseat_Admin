152e930b92ac020c7ae8a89ae858ba88fc6399a1
C-lento <161337636+C-LentoSC@users.noreply.github.com>
2025-02-03 21:13:53 +0530

    Update ManageFare.js

--- src/components/ManageFare.js
+++ src/components/ManageFare.js
@@ -70,13 +70,13 @@ const ManageFare = () => {
     };
 
     const handleFareChange = (id, value) => {
-        const regex = /^(?:\d{0,2})(?:\.\d{0,2})?$/;
+        const regex = /^\d{0,2}(\.\d*)?$/;
 
-    if (value === "" || regex.test(value)) {
-        setRoutes(routes.map(route =>
-            route.id === id ? { ...route, newFare: value } : route
-        ));
-    }
+        if (value === "" || regex.test(value)) {
+            setRoutes(routes.map(route =>
+                route.id === id ? { ...route, newFare: value } : route
+            ));
+        }
     };
 
     const handleExport = () => {
@@ -320,7 +320,7 @@ const ManageFare = () => {
                                         <TableCell sx={{ py: 0 }} align="right">
                                             <TextField
                                                 disabled
-                                                value={`LKR ${route.oldFare}`}
+                                                value={`LKR ${route.oldFare ? parseFloat(route.oldFare).toFixed(2) : "0.00"}`}
                                                 size="small"
                                                 sx={{ width: 250 }}
                                             />
@@ -328,7 +328,7 @@ const ManageFare = () => {
                                         <TableCell sx={{ py: 0 }} align="center">=</TableCell>
                                         <TableCell sx={{ py: 0 }} align="right">
                                             <TextField
-                                                value={route.newFare}
+                                                value={route.newFare ? parseFloat(route.newFare).toFixed(2) : "0.00"}
                                                 onChange={(e) => handleFareChange(route.id, e.target.value)}
                                                 size="small"
                                                 sx={{ width: 250 }}
